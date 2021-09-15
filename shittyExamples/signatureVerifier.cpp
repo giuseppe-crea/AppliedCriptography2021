75b5c0e82beb82a9df73b6f34a70e3ea53cce819
Sylt556 <g.crea2@studenti.unipi.it>
2021-09-15 20:21:53 +0200

    check shittyExamples/signatureVerifier

--- shittyExamples/signatureVerifier.cpp
+++ shittyExamples/signatureVerifier.cpp
@@ -1,81 +1,96 @@
-#include "../client/signature_utilities.cpp"
+#include "../client/Message.cpp"
+#include "../server/getKeys.cpp"
 
 using namespace std;
+
+
 
 int serialize_cert(X509* cert, BIO** return_val){
     *return_val = BIO_new(BIO_s_mem());
     return PEM_write_bio_X509(*return_val, cert);
 }
 
-int main(int argc, char const *argv[])
-{
-    // load server cert
-	FILE *fp_SV_cert = fopen("../certificates/serv_cert.pem", "r"); 
-	if(!fp_SV_cert){
-		perror("SV certificate pem file");
-		exit(-1);
-	}
-	X509* SV_cert = PEM_read_X509(fp_SV_cert, NULL, NULL, NULL);
-	fclose(fp_SV_cert);
-
-    X509_STORE* store = X509_STORE_new();
-	FILE *fp_CA_cert = fopen("../client/keys/ca_cert.pem", "r"); 
-	if(!fp_CA_cert){
-		perror("CA certificate pem file");
-		exit(-1);
-	}
-	X509* CA_cert = PEM_read_X509(fp_CA_cert, NULL, NULL, NULL);
-	X509_STORE_add_cert(store, CA_cert);
-	fclose(fp_CA_cert);
-
-    X509_STORE_CTX* ctx = X509_STORE_CTX_new();
-	X509_STORE_CTX_init(ctx, store, SV_cert, NULL);
-
-    // sender
-    BIO* buffer;
-    serialize_cert(SV_cert, &buffer);
-    unsigned char* send_buffer;
-    long send_size = BIO_get_mem_data(buffer, &send_buffer);
-    
-    // receiver
-    unsigned char* receive_buffer = new unsigned char[send_size];
-    memcpy(receive_buffer, send_buffer, send_size);
-    BIO* new_buffer = BIO_new(BIO_s_mem());
-    BIO_write(new_buffer, receive_buffer, send_size);
-    X509* new_server_cert;
-    new_server_cert = PEM_read_bio_X509(new_buffer, NULL, 0, NULL);
-
-	if(X509_verify_cert(ctx)){ // verifies serv_cert based on the context previously created
-		EVP_PKEY* sv_pub_key = X509_get_pubkey(new_server_cert);
-        /*
-        unsigned char* printbuffer = new unsigned char[512];
-        memcpy(printbuffer, sv_pub_key, 512);
-        for(int ieti = 0; ieti < 512; ieti++){
-			cout << (int)printbuffer[ieti];
-			if(ieti == 511) 
-				cout << endl;
-		}
-        */
-        EVP_PKEY* sv_prv_key;
-        FILE* pem_sv_prvkey = fopen("../certificates/serv_prvkey.pem","r");
-	    sv_prv_key = PEM_read_PrivateKey(pem_sv_prvkey,NULL,NULL,NULL);
-        X509_STORE_CTX_free(ctx);
-        unsigned char pt[9] = "ciao";
-        int32_t madonna = 3;
-        memcpy(pt+5, &madonna, 4);
-
-        unsigned char* signature_value;
-        unsigned int sig_len;
-        int ret1 = signature(sv_prv_key, pt, &signature_value, 9, &sig_len);
-        unsigned char data[5] = "ciao";
-        unsigned char* signature_to_verify = (unsigned char*)calloc(sig_len, sizeof(unsigned char));
-        memcpy(signature_to_verify, signature_value, sig_len);
-        int ret3 = memcmp(signature_to_verify, signature_value, sig_len);
-        printf("sig_len = %d\n", sig_len);
-        printf("RET3 = %d\n", ret3);
-        int ret2 = verify_sign(sv_pub_key, data, madonna, 5, signature_to_verify, sig_len);
-        printf("RET1 = %d\nRET2 = %d\n", ret1, ret2);
+void on_screen(EVP_PKEY* key){
+    int len_of_key = EVP_PKEY_size(key);
+    unsigned char* buffer = (unsigned char*)calloc(len_of_key, sizeof(unsigned char));
+    memcpy(buffer, key, len_of_key);
+    for(int i = 0; i < len_of_key; i++){
+        cout << (int)buffer[i] << " ";
     }
+    cout << endl;
+}
+
+void on_screen_bio(unsigned char* key, long size){
+    unsigned char* buffer = (unsigned char*)calloc(size, sizeof(unsigned char));
+    memcpy(buffer, key, size);
+    for(int i = 0; i < size; i++){
+        cout << (int)buffer[i] << " ";
+    }
+    cout << endl;
+}
+
+int peer_public_key_msg(unsigned char* data, int data_dim, EVP_PKEY** peer_public_key){
+    //stores the public key automatically sent by server
+    long pem_dim;
+    BIO* peer_pub_key_pem = BIO_new(BIO_s_mem());
+    memcpy(&pem_dim, data, sizeof(long));
+    unsigned char * buffer = (unsigned char*)calloc(pem_dim, sizeof(unsigned char));
+    memcpy(buffer, data+sizeof(long), pem_dim);
+    memcpy(buffer+pem_dim-1, "\0", 1);
+
+    int ret = BIO_write(peer_pub_key_pem, buffer, pem_dim);
+    cout << "Printing second key:" << endl;
+    on_screen_bio(buffer, pem_dim);
+    free(buffer);
+
+    if(data_dim != pem_dim+sizeof(long))
+        printf("Bad data!!\n");
+
+    *peer_public_key = PEM_read_bio_PUBKEY(peer_pub_key_pem, NULL, NULL, NULL);
+    return ret;
+}
+
+Message* send_peer_pubkey(){
+    string username = "alice";
+    EVP_PKEY* public_key;
+    get_keys(username, &public_key);
+    Message* reply = new Message();
+    int32_t ret = 0;
+    ret += reply->SetOpCode(100);
+    ret += reply->SetCounter(0);
+    // the public key must be shared as BIO
+    BIO* pubkey_bio = BIO_new(BIO_s_mem());
+    unsigned char* pubkey_buffer;
+    PEM_write_bio_PUBKEY(pubkey_bio, public_key);
+    // place it on an unsigned char buffer and get its length
+    long pem_size = BIO_get_mem_data(pubkey_bio, &pubkey_buffer);
+    // debug print
+    cout << "Printing first key:" << endl;
+    on_screen_bio(pubkey_buffer, pem_size);
+    // concat length + key in an unsigned char buffer
+    unsigned char* send_buffer = (unsigned char*)calloc(pem_size+sizeof(long), sizeof(unsigned char));
+    memcpy(send_buffer,&pem_size, sizeof(long));
+    memcpy(send_buffer+sizeof(long), pubkey_buffer, pem_size);
+
+    // finally add it to the message
+    ret += reply->setData(send_buffer, pem_size+sizeof(long));
+    BIO_free(pubkey_bio);
+    free(send_buffer);
+    if(ret != 0){
+        // an error occurred communicating with Bob, telling Alice the request was denied
+        delete(reply);
+        return NULL;
+    }
+    return reply;
+}
+
+int main(int argc, char const *argv[])
+{   int datadim;
+    EVP_PKEY* received_key;
+    Message* fake_msg = send_peer_pubkey();
+    unsigned char* data = fake_msg->getData(&datadim);
+
+    peer_public_key_msg(data, datadim, &received_key);
 
     return 0;
 }
