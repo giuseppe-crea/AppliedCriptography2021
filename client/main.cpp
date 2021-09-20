2fa1101d8787f821a4ef2d2652dfbe31d198ca2f
m4r-kus <parti.marco@gmail.com>
2021-09-20 19:59:26 +0200

    fixing client

--- client/main.cpp
+++ client/main.cpp
@@ -110,6 +110,8 @@ int main(int argc, char **argv){
 
 	cout << "Connected" << endl;
 
+
+
 	//authentication of the client
 	auth(cl_id, sessionVariables->cl_prvkey, sessionVariables->cl_pubkey, sessionVariables->sockfd, &(sessionVariables->sv_session_key), store);
   
